9f0bc2a6d42d924f7dd8f333a757e97f066eb082
llovujw <147962746+llovujw@users.noreply.github.com>
2023-11-03 06:26:56 +0700

    Add files via upload

---
+++ struktur kondisi/Latihan 1.py
@@ -0,0 +1,8 @@
+bil1 = input("Masukan bilangan : ")
+bil2 = input("Masukan bilangan : ")
+
+if (bil1 > bil2):
+   print("Bilangan terbesar :",bil1)
+
+if (bil1 < bil2):
+   print("Bilangan terbesar :",bil2)
