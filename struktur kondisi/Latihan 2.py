166301a2641b7ddb277ac9c221c31205b0097794
llovujw <147962746+llovujw@users.noreply.github.com>
2023-11-03 06:26:16 +0700

    Add files via upload

---
+++ struktur kondisi/Latihan 2.py
@@ -0,0 +1,8 @@
+bil1 = input("Bilangan ke-1: ")
+bil2 = input("Bilangan ke-2: ")
+bil3 = input("Bilangan ke-3: ")
+
+data = [bil1, bil2, bil3]
+
+list.sort(data)
+print("Data dari terkecil :", data)
